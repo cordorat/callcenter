9766cc61b3966dc30f35e6726f6c81b3bce5f020
Sara Sinisterra Villota <sinisterra.sara@correounivalle.edu.co>
2025-10-09 23:28:35 -0500

    Conexión de Campaña con el backend

---
+++ frontend/src/components/campaing/Teams.jsx
@@ -0,0 +1,17 @@
+//Path: src/components/campaing/Teams.jsx
+//Componente para mostrar la lista de equipos de la campaña
+
+import * as React from "react";
+import { Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Chip } from '@mui/material';
+
+
+export default function Teams() {
+  return (
+    <Box>
+      <Typography variant="h6" gutterBottom>
+        Lista de Equipos
+      </Typography>
+
+    </Box>
+  );
+}
