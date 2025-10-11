8161b039de2f012d53baf7501cf57dec9d862f5f
Sara Sinisterra Villota <sinisterra.sara@correounivalle.edu.co>
2025-10-10 22:05:31 -0500

    Cambio de lugar el snackbar de los cambios de estado

--- frontend/src/components/agentStatus/AgentStatus.jsx
+++ frontend/src/components/agentStatus/AgentStatus.jsx
@@ -300,7 +300,7 @@ export default function AgentStatus({ onStatusChange, refreshInterval = 30000 })
         open={snackbar.open} 
         autoHideDuration={4000} 
         onClose={handleCloseSnackbar}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
       >
         <Alert 
           onClose={handleCloseSnackbar} 
