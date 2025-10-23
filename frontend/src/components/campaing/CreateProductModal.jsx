450c2090b8bd1ad652da94b2cccb16f65dc2140a
crisgit24 <cristian.camilo.franco@correounivalle.edu.co>
2025-10-22 21:16:40 -0500

    pequeño arreglo crear modal

--- frontend/src/components/campaing/CreateProductModal.jsx
+++ frontend/src/components/campaing/CreateProductModal.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Box, Button, Input, Modal, TextField, Typography } from "@mui/material";
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 
 export default function BasicModal() {
