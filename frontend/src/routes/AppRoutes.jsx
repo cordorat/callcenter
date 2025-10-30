bb40fe4dd66c03e772483ac04f4956d0d12adfce
Sara Sinisterra Villota <sinisterra.sara@correounivalle.edu.co>
2025-10-29 22:25:40 -0500

    Frontend de perfil y dashboard de coordinador sin conectar

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -15,6 +15,7 @@ import CrearUsuario from '../pages/Usuario/CrearUsuario';
 import AgentesPage from '../pages/Kpis/AgentesPage';
 import AgentDetailPage from '../pages/Kpis/AgentDetailPage';
 import HistorialLlamadas from '@/pages/Historial/HistorialLlamadas';
+import ProfilePage from '@/pages/Profile/ProfilePage';
 
 const AppRoutes = () => {
   const { isAuthenticated } = useAuth();
@@ -29,6 +30,8 @@ const AppRoutes = () => {
         />
         
         {/* Rutas protegidas */}
+
+        {/* Ruta Dashboard */}
         <Route 
           path="/dashboard" 
           element={
@@ -38,6 +41,7 @@ const AppRoutes = () => {
           } 
         />
 
+        {/* Ruta Llamadas */}
         <Route 
           path="/llamadas" 
           element={
@@ -47,6 +51,7 @@ const AppRoutes = () => {
           } 
         />
         
+        {/* Ruta Historial de Llamadas */}
          <Route
           path="/historial"
           element={
@@ -56,6 +61,7 @@ const AppRoutes = () => {
           }
         />
 
+        {/* Ruta KPIs */}
         <Route
           path="/kpis"
           element={
@@ -84,33 +90,38 @@ const AppRoutes = () => {
           }
         />
 
+        {/* Rutas de Campaña */}
         <Route path="/campana" element={
           <PrivateRoute>
             <Campaing />
           </PrivateRoute>
         } />
 
+        {/* Rutas de Usuarios */}
         <Route path="/usuarios" element={
           <PrivateRoute>
             <Usuarios />
           </PrivateRoute>
-        } />
+        } />  
 
-        <Route path="/crear-usuario" element={
-          <PrivateRoute>
-            <CrearUsuario />
-          </PrivateRoute>
-        } />        
-
+        {/* Ruta para equipos */}
         <Route path="/equipos" element={
           <PrivateRoute>
             <Teams />
           </PrivateRoute>
         } />
 
+        {/* Ruta para configuracion */}
         <Route path="/configuracion" element={
           <PrivateRoute>
             <Settings />
+          </PrivateRoute>
+        } />
+
+        {/* Ruta para perfil */}
+        <Route path="/perfil" element={
+          <PrivateRoute>
+            <ProfilePage />
           </PrivateRoute>
         } />
 
